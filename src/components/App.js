981641c9f8b57a202335878436de3befcdbee476
helciodev <helcioandre@live.com>
2021-08-25 11:38:22 +0100

    Create components and attach App component to main index.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BooksForm from 'BooksForm';
+import BooksList from 'BooksList';
+
+function App() {
+  return (
+    <div>
+      <BooksList />
+      <BooksForm />
+    </div>
+  );
+}
+
+export default App;
