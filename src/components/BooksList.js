fca865538ff902ed47f0fee0d4a7a159aa4d985b
helciodev <helcioandre@live.com>
2021-08-25 11:38:22 +0100

    Create components and attach App component to main index.js

---
+++ src/components/BooksList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BooksList() {
+
+}
+
+export default BooksList;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const hello = <h1>Hello, bookstore cms</h1>;
 ReactDOM.render(
-  hello,
+  <App />,
   document.getElementById('root'),
 );
 
