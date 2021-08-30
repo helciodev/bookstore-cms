2a2b90fbb5d49d3e94bae1051780a5de5fae04c5
helciodev <helcioandre@live.com>
2021-08-30 15:24:35 +0100

    Set state for BookForm component

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function BooksForm() {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    if (name === 'title') {
+      setTitle(value);
+    } else if (name === 'category') {
+      setCategory(value);
+    }
+  }
+
   const collection = [
     '--categories--',
     'Action',
@@ -11,11 +23,17 @@ function BooksForm() {
     'Learning',
     'Sci-fi',
   ];
+
+  console.log(title, category);
   return (
     <div>
       <form action="">
-        <input type="text" placeholder="book title" />
-        <select>
+        <input type="text" name="title" value={title} onChange={handleChange} placeholder="book title" />
+        <select
+          value={category}
+          name="category"
+          onChange={handleChange}
+        >
           {collection.map((category) => (
             <option key={category} value={category}>
               {' '}
